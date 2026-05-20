45764c099caa25320b06c2f20affb12fc8272407
talosigsanjoreyvien-netizen <talosigsanjoreyvien@gmail.com>
2026-05-20 22:30:44 +0800

    refactor: Update build scripts and project settings
    Updates Gradle build scripts to include essential plugins and dependencies. Also, renames the root project to "UNIBLOX Browser" and adjusts repository resolution mode for better dependency management.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+  repositories {
+    google()
+    mavenCentral()
+  }
+  dependencies {
+    classpath("com.android.tools.build:gradle:9.1.1")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.10")
+    classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+  }
+}
+
 plugins {
   id("com.android.application") version "9.1.1" apply false
   id("org.jetbrains.kotlin.android") version "2.2.10" apply false
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,13 +15,13 @@ pluginManagement {
 plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
 
 dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
   repositories {
     google()
     mavenCentral()
   }
 }
 
-rootProject.name = "My Application"
+rootProject.name = "UNIBLOX Browser"
 
 include(":app")
